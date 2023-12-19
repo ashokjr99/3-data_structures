434dd16d5814b3eaef40c5e6fb7965f4e8b96395
ashokjr99 <ashoksharmajr@gmail.com>
2023-12-18 18:42:51 -0600

    states

---
+++ state/index.js
@@ -0,0 +1,92 @@
+// function getPoemTitle(authorUserSelected) {
+//   let poemTitlesByAuthor = {
+//     "Robert Frost": "Stopping by Woods on a Snowy Evening",
+//     "Shel Silverstein": "Falling Up",
+//     "Sylvia Plath": "The Bell Jar",
+//   };
+
+//   return poemTitlesByAuthor[authorUserSelected];
+// }
+
+// console.log(getPoemTitle("Sylvia Plath"));
+
+// [🟢] → [🟡] → [🔴] ↤↦ [⚠️]
+//   ↖⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽」
+
+let states = {
+  green: ["yellow"],
+  yellow: ["red"],
+  red: ["green", "yield"],
+  yield: ["red"],
+};
+
+let currentState = "green";
+
+function enterState(newState) {
+  let validTransitions = states[currentState];
+  if (validTransitions.includes(newState)) {
+    currentState = newState;
+    console.log("The state has changed to: ", currentState);
+  } else {
+    throw (
+      "Invalid state transition attempted - from " +
+      currentState +
+      " to " +
+      newState
+    );
+  }
+}
+
+// enterState("red") //! Hits the throw / else block
+
+enterState("yellow");
+enterState("red");
+// enterState("yellow") //! hits throw / else block
+enterState("green");
+
+// -------------------------------------------------------------------------------------------
+
+// Objects are nouns, and represent things in the program
+// Methods are verbs, and represent actions the objects do
+// Properties are adjectives, they describe the object as a noun
+// Classes are categories, something that describes a type of thing, not a thing itself
+
+class Animal {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  sleep() {
+    return `${this.name} is going to sleep....`;
+  }
+
+  describe() {
+    return `Hi there, I am ${this.name}, my color is ${this.color}!`;
+  }
+}
+
+// const monkey = new Animal("monkey", "tan");
+
+// console.log(monkey.sleep());
+// console.log(monkey.describe());
+
+class Dog extends Animal {
+  constructor(name, color) {
+    super(name, color);
+  }
+
+  //  .sleep()  //? Method comes from parent class
+
+  // Overwriting the inherited method
+  describe() {
+    return `A ${this.color} colored dog named ${this.name} says Woof!`;
+  }
+}
+
+const doggy = new Dog("fido", "brown");
+const monkey = new Animal("Curious George", "brown");
+
+console.log(doggy.describe());
+
+console.log(monkey.describe()); //? Calling new method
